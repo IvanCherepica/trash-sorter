df1196ed7eb6fd10b9bd76f03d36ceeece69701d
khilkevich igor <khilkevichigor@gmail.com>
2019-08-30 18:31:39 +0300

    add QrCodeService

---
+++ src/main/java/com/trash_sorter/service/QrService.java
@@ -0,0 +1,33 @@
+package com.trash_sorter.service;
+
+import com.trash_sorter.util.PropertyReader;
+
+public class QrService {
+
+    private PropertyReader reader;
+
+    public QrService() {
+        this.reader = new PropertyReader("qrPROP.properties");
+    }
+
+    public String getQrUrl(long tankId, int size) {
+
+        String url = reader.getProperty("linkRequest") +
+                reader.getProperty("serverPath") +
+                "/tank?id=" +
+                tankId +
+                reader.getProperty("preSizeRequest_1") +
+                size +
+                reader.getProperty("preSizeRequest_2") +
+                size +
+                reader.getProperty("endRequest");
+
+        return url;
+    }
+
+    public static void main(String[] args) {
+        QrService service = new QrService();
+
+        System.out.println(service.getQrUrl(1, 250));
+    }
+}
--- src/main/java/com/trash_sorter/servlet/TankServlet.java
+++ src/main/java/com/trash_sorter/servlet/TankServlet.java
@@ -13,12 +13,13 @@ public class TankServlet extends HttpServlet {
     public static long id;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String string_id = req.getParameter("id");
+        String string_id = req.getParameter("id");
 //        id = Long.parseLong(string_id);
         //получить название бака
 
 //        TankService service = TankServiceIMPL.getInstance();
 //        String tankName = service.getTankById(id).getTankName();
+
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
         dispatcher.forward(req,resp);
